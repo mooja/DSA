359f96842aa26118a323df3984327ad0dc2fd1f1
Max Shkurygin <max.atreides@gmail.com>
2017-07-04 12:34:02 -0400

    basic api

---
+++ priority_queue/pq.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class PQ(object):
+
+    """An implementation of Max Priority Queue"""
+
+    def __init__(self):
+        self.pq = [None]
+        self.size = 0
+
+    def del_max(self):
+        pass
+
+    def insert(self, key):
+        pass
+
+    def _exch(self, idx1, idx2):
+        pass
+
+    def _sink(self, idx):
+        pass
+
+    def _swim(self, idx):
+        pass
