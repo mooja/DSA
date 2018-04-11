2bf339be4ad96a61399d4de9a78d9ba182bec324
Max Shkurygin <max.atreides@gmail.com>
2018-04-11 18:30:13 -0400

    symbol table basic api

---
+++ symbol_table/SymbolTable.ts
@@ -0,0 +1,14 @@
+class SymbolTable<Key, Value> {
+    /* Inteface Definition */
+    put(key: Key, value: Value): void {
+
+    }
+
+    get(key: Key): Value {
+
+    }
+
+    contains(key: Key): Boolean {
+
+    }
+}
